574266ef8eb69b7f04b1098ad18ea6e70187b9c4
tasya <taisiya_struchaeva@mail.ru>
2022-02-21 19:47:32 +0300

    Lesson6

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        ReadingAString readingAString = new ReadingAString();
+        readingAString.readingString();
+    }
+}
+
