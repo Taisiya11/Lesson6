6183049981f9cf14ac52f2879ccd67dd5a055ae0
tasya <taisiya_struchaeva@mail.ru>
2022-02-22 11:29:41 +0300

    Exercise_2

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args){
+        FileReader fileReader = new FileReader();
+        fileReader.readingFile();
+    }
+}
